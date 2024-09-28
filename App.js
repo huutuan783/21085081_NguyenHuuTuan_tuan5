22102f3f53b454ba07b620b38c833e0d0fb7bf3c
huutuan783 <tuanhuu783@gmail.com>
2024-09-28 19:03:20 +0700

    done

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen_01 from './screens/Screen_01';
+import Screen_02 from './screens/Screen_02';
+import Screen_03 from './screens/Screen_03';
+import Screen_04 from './screens/Screen_04';
+import { AccountProvider } from './ContextAPI/AccountContext';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AccountProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Screen_01"
+            component={Screen_01}
+            options={{
+              title: '', 
+            }}
+          />
+          <Stack.Screen
+            name="Screen_02"
+            component={Screen_02}
+            options={{
+              title: '',  
+              headerTintColor: '#000',
+            }}
+          />
+          <Stack.Screen
+            name="Screen_03"
+            component={Screen_03}
+            options={{
+              title: '',  
+              headerTintColor: '#000',
+            }}
+          />
+          <Stack.Screen
+            name="Screen_04"
+            component={Screen_04}
+            options={{
+              title: 'Product name',  
+              headerTintColor: '#000',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AccountProvider>
   );
 }
 
